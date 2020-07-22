f87bffe325031a856fb60b0564d4ad893051ad66
wabrams <wabrams@users.noreply.github.com>
2020-07-22 14:40:46 -0600

    housekeeping

--- timer_frequency_generation/src/main.c
+++ timer_frequency_generation/src/main.c
@@ -1,20 +1,3 @@
-/**************************************************************************//**
- * @main.c
- * @brief This project demonstrates frequency generation using the
- * TIMER module. PA6 (Expansion Header Pin 14) is configured for output
- * compare and toggles PA6 on each overflow event at a set frequency.
- * @version 0.0.1
- ******************************************************************************
- * @section License
- * <b>Copyright 2020 Silicon Labs, Inc. http://www.silabs.com</b>
- *******************************************************************************
- *
- * This file is licensed under the Silabs License Agreement. See the file
- * "Silabs_License_Agreement.txt" for details. Before using this software for
- * any purpose, you must agree to the terms of that agreement.
- *
- ******************************************************************************/
-
 #include "em_device.h"
 #include "em_chip.h"
 #include "em_cmu.h"
@@ -22,85 +5,63 @@
 #include "em_gpio.h"
 #include "em_timer.h"
 
-// Desired frequency in Hz
-// Min: 145 Hz, Max: 9.5 MHz with default settings
-#define OUT_FREQ 1000
+#define OUT_FREQ 1000				/**< Output Frequency (Hz) **/
+#define REFERENCE_INVERT 1			/**< Compiler Macro, based on speaker configuration **/
 
-/**************************************************************************//**
- * @brief GPIO initialization
- *****************************************************************************/
-void initGPIO(void)
+void initCMU(void)
 {
-  GPIO_PinModeSet(gpioPortD, 2, gpioModePushPull, 0);
-  GPIO_PinModeSet(gpioPortD, 3, gpioModePushPull, 0);
-}
-
-/**************************************************************************//**
- * @brief
- *    CMU initialization
- *****************************************************************************/
-void initCmu(void)
-{
-  // Enable clock to GPIO and TIMER0
   CMU_ClockEnable(cmuClock_GPIO, true);
   CMU_ClockEnable(cmuClock_TIMER0, true);
 }
 
-/**************************************************************************//**
- * @brief TIMER initialization
- *****************************************************************************/
+void initGPIO(void)
+{
+  GPIO_PinModeSet(gpioPortD, 3, gpioModePushPull, 0);
+  // [<X>].CC<Y> : X is TIMER#, Y is CH#
+  GPIO->TIMERROUTE[0].CC0ROUTE = (gpioPortD << _GPIO_TIMER_CC0ROUTE_PORT_SHIFT) | (3 << _GPIO_TIMER_CC0ROUTE_PIN_SHIFT);
+  #if REFERENCE_INVERT
+    GPIO_PinModeSet(gpioPortD, 2, gpioModePushPull, 0);
+	GPIO -> TIMERROUTE[0].CC1ROUTE = (gpioPortD << _GPIO_TIMER_CC1ROUTE_PORT_SHIFT) | (2 << _GPIO_TIMER_CC1ROUTE_PIN_SHIFT);
+	GPIO -> TIMERROUTE[0].ROUTEEN  = GPIO_TIMER_ROUTEEN_CC0PEN | GPIO_TIMER_ROUTEEN_CC1PEN;
+  #else
+	GPIO -> TIMERROUTE[0].ROUTEEN  = GPIO_TIMER_ROUTEEN_CC0PEN;
+  #endif
+}
+
 void initTIMER(void)
 {
-  uint32_t timerFreq = 0;
-
-  // Initialize the timer
+  // Initialize TIMER0
   TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
 	  timerInit.prescale = timerPrescale1;
 	  timerInit.enable = false;
 	  timerInit.debugRun = false;
-
-  // Configure TIMER0 Compare/Capture for output compare
+  TIMER_Init(TIMER0, &timerInit);
+  // Initialize TIMER0 Compare / Capture
   TIMER_InitCC_TypeDef timerCCInit = TIMER_INITCC_DEFAULT;
 	  timerCCInit.mode = timerCCModeCompare;
 	  timerCCInit.cmoa = timerOutputActionToggle;
-
-  // configure, but do not start timer
-  TIMER_Init(TIMER0, &timerInit);
-
-  // Route Timer0 CC0 output to PA6
-  GPIO->TIMERROUTE[0].ROUTEEN  = GPIO_TIMER_ROUTEEN_CC0PEN | GPIO_TIMER_ROUTEEN_CC1PEN;
-  GPIO->TIMERROUTE[0].CC0ROUTE = (gpioPortD << _GPIO_TIMER_CC0ROUTE_PORT_SHIFT)
-  								  | (3 << _GPIO_TIMER_CC0ROUTE_PIN_SHIFT);
-  // TODO: is this complement?
-  GPIO -> TIMERROUTE[0].CC1ROUTE = (gpioPortD << _GPIO_TIMER_CC1ROUTE_PORT_SHIFT)
-  		  								  | (2 << _GPIO_TIMER_CC1ROUTE_PIN_SHIFT);
   TIMER_InitCC(TIMER0, 0, &timerCCInit);
-  	  timerCCInit.outInvert = true;
-  TIMER_InitCC(TIMER0, 1, &timerCCInit);
-
-  // Set Top value
-  // Note each overflow event constitutes 1/2 the signal period
-  timerFreq = CMU_ClockFreqGet(cmuClock_TIMER0)/(timerInit.prescale + 1);
+  // Initialize TIMER0 Compare / Capture Invert
+  #if REFERENCE_INVERT
+  	timerCCInit.outInvert = true;
+    TIMER_InitCC(TIMER0, 1, &timerCCInit);
+  #endif
+  // Initialize Top Value, note each compare event is 1/2 the signal period
+  uint32_t timerFreq = CMU_ClockFreqGet(cmuClock_TIMER0) / (timerInit.prescale + 1);
   int topValue = timerFreq / (2*OUT_FREQ) - 1;
   TIMER_TopSet (TIMER0, topValue);
-
-  /* Start the timer */
-  TIMER_Enable(TIMER0, true);
 }
 
-/**************************************************************************//**
- * @brief  Main function
- *****************************************************************************/
 int main(void)
 {
-  // Chip errata
   CHIP_Init();
-
-  // Initializations
-  initCmu();
+  // Initialize Hardware
+  initCMU();
   initGPIO();
   initTIMER();
-
+  // Start Timer
+  TIMER_Enable(TIMER0, true);
+  // Infinite Loop
   while (1)
   {
 	  EMU_EnterEM1();
