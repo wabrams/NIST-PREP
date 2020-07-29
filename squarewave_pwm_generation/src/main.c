4bb0805d186047ad2d56df5b95371b179e68edac
wabrams <wabrams@users.noreply.github.com>
2020-07-28 19:39:34 -0600

    button singleshot
    backup before modifications

--- squarewave_pwm_generation/src/main.c
+++ squarewave_pwm_generation/src/main.c
@@ -16,50 +16,74 @@
 static uint32_t topValue = 0;
 volatile int pulse_width= PWM_FREQ / 400;  // this sets the pulse width to 2.5mS (1/400)
 
+typedef enum squarechirp_mode_e
+{
+  chirpInc,
+  chirpOn,
+  chirpDec,
+  chirpOff
+} squarechirp_mode_t;
+
+static volatile squarechirp_mode_t chirp_mode = chirpOff;
+static volatile uint32_t chirp_cnt = 0;
 static volatile float dutyCycle = 0;
-volatile bool on_off = true;
 
 void button0Callback(uint8_t pin)
 {
-	on_off = !on_off;
-	TIMER_Enable(TIMER0, on_off);
+  chirp_mode = chirpInc;
+	GPIO_PinOutToggle(gpioPortD, 2);
 }
 
-/**************************************************************************//**
- * @brief
- *    Interrupt handler for TIMER0 that changes the duty cycle
- *
- * @note
- *    This handler doesn't actually dynamically change the duty cycle. Instead,
- *    it acts as a template for doing so. Simply change the dutyCycle
- *    global variable here to dynamically change the duty cycle.
- *****************************************************************************/
 void TIMER0_IRQHandler(void)
 {
+  uint32_t flags = TIMER0 -> IF_CLR = TIMER0 -> IF;
   // Acknowledge the interrupt
-  uint32_t flags = TIMER_IntGet(TIMER0);
-  static int count=0;
-  TIMER_IntClear(TIMER0, flags);
-  if ((dutyCycle<TARGET_DUTY_CYCLE)&&(on_off)) {
-	  dutyCycle += DUTY_CYCLE_STEPS;
+  if (flags & TIMER_IF_CC1)
+  {
+    switch (chirp_mode)
+    {
+      case chirpInc:
+        dutyCycle += DUTY_CYCLE_STEPS;
+        if (dutyCycle >= TARGET_DUTY_CYCLE)
+        {
+          chirp_mode = chirpOn;
+          dutyCycle = TARGET_DUTY_CYCLE;
+        }
+        TIMER_CompareBufSet(TIMER0, 1, (uint32_t)(topValue * dutyCycle));
+        break;
+      case chirpOn:
+        chirp_cnt++;
+        if (chirp_cnt >= pulse_width)
+            chirp_mode = chirpDec;
+        break;
+      case chirpDec:
+        dutyCycle -= DUTY_CYCLE_STEPS;
+        if (dutyCycle <= 0)
+        {
+          chirp_mode = chirpOff;
+          dutyCycle = 0;
+        }
+        TIMER_CompareBufSet(TIMER0, 1, (uint32_t)(topValue * dutyCycle));
+        break;
+      case chirpOff:
+        chirp_cnt = 0;
+
+        //TODO: disable timer here
+        break;
+    }
   }
-  if ((dutyCycle>0)&&(!on_off)) {
-	  dutyCycle -= DUTY_CYCLE_STEPS;
-  }
-  if (on_off) {
-	  count++;
-	  if (count==pulse_width) {
-		  on_off = false;
-		  count=0;
-	  }
-  }
-  // Update CCVB to alter duty cycle starting next period
-  TIMER_CompareBufSet(TIMER0, 0, (uint32_t)(topValue * dutyCycle));
 }
 
 void initGPIO(void)
 {
+  //gpio
+  GPIO_PinModeSet(gpioPortD, 2, gpioModePushPull, 0);
   GPIO_PinModeSet(gpioPortD, 3, gpioModePushPull, 0);
+  //timer
+  GPIO->TIMERROUTE[0].ROUTEEN  = GPIO_TIMER_ROUTEEN_CC0PEN | GPIO_TIMER_ROUTEEN_CC1PEN;
+//  GPIO->TIMERROUTE[0].CC0ROUTE = (gpioPortD << _GPIO_TIMER_CC0ROUTE_PORT_SHIFT) | (2 << _GPIO_TIMER_CC0ROUTE_PIN_SHIFT);
+  GPIO->TIMERROUTE[0].CC1ROUTE = (gpioPortD << _GPIO_TIMER_CC1ROUTE_PORT_SHIFT) | (3 << _GPIO_TIMER_CC1ROUTE_PIN_SHIFT);
+  //button
   GPIO_PinModeSet(BSP_BUTTON0_PORT, BSP_BUTTON0_PIN, gpioModeInputPullFilter, 1);
   GPIOINT_Init();
   GPIOINT_CallbackRegister(BSP_BUTTON0_PIN, button0Callback);
@@ -72,48 +96,36 @@ void initCMU(void)
   CMU_ClockEnable(cmuClock_TIMER0, true);
 }
 
-void initTimer(void)
+void initTIMER(void)
 {
-  uint32_t timerFreq = 0;
-  // Initialize the timer
-  TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
-  // Configure TIMER0 Compare/Capture for output compare
-  TIMER_InitCC_TypeDef timerCCInit = TIMER_INITCC_DEFAULT;
 
-  // Use PWM mode, which sets output on overflow and clears on compare events
-  timerInit.prescale = timerPrescale64;
-  timerInit.enable = false;
-  timerCCInit.mode = timerCCModePWM;
+  TIMER_Init_TypeDef timer0Init = TIMER_INIT_DEFAULT;
+    timer0Init.prescale = timerPrescale64;
+    timer0Init.enable = false;
+    timer0Init.debugRun = false;
+    timer0Init.riseAction = timerInputActionReloadStart;
+  TIMER_Init(TIMER0, &timer0Init);
 
-  // Configure but do not start the timer
-  TIMER_Init(TIMER0, &timerInit);
+  TIMER_InitCC_TypeDef timer0CC0Init = TIMER_INITCC_DEFAULT;
+    timer0CC0Init.mode = timerCCModeCapture;
+  TIMER_InitCC(TIMER0, 0, &timer0CC0Init);
 
-  // Route Timer0 CC0 output to PA6
-  GPIO->TIMERROUTE[0].ROUTEEN  = GPIO_TIMER_ROUTEEN_CC0PEN;
-  GPIO->TIMERROUTE[0].CC0ROUTE = (gpioPortD << _GPIO_TIMER_CC0ROUTE_PORT_SHIFT)
-  								  | (3 << _GPIO_TIMER_CC0ROUTE_PIN_SHIFT);
-
-  // Configure CC Channel 0
-  TIMER_InitCC(TIMER0, 0, &timerCCInit);
+  TIMER_InitCC_TypeDef timer0CC1Init = TIMER_INITCC_DEFAULT;
+    timer0CC1Init.mode = timerCCModePWM;
+  TIMER_InitCC(TIMER0, 1, &timer0CC1Init);
 
   // Start with 10% duty cycle
   dutyCycle = DUTY_CYCLE_STEPS;
 
-  // set PWM period
-  timerFreq = CMU_ClockFreqGet(cmuClock_TIMER0) / (timerInit.prescale + 1);
+  uint32_t timerFreq = CMU_ClockFreqGet(cmuClock_TIMER0) / (timer0Init.prescale + 1);
   topValue = (timerFreq / PWM_FREQ);
-  // Set top value to overflow at the desired PWM_FREQ frequency
   TIMER_TopSet(TIMER0, topValue);
+  TIMER_CompareSet(TIMER0, 1, (uint32_t)(topValue * dutyCycle));
 
-  // Set compare value for initial duty cycle
-  TIMER_CompareSet(TIMER0, 0, (uint32_t)(topValue * dutyCycle));
-
-  // Start the timer
-  TIMER_Enable(TIMER0, true);
-
-  // Enable TIMER0 compare event interrupts to update the duty cycle
-  TIMER_IntEnable(TIMER0, TIMER_IEN_CC0);
+  TIMER_IntEnable(TIMER0, TIMER_IEN_CC1);
   NVIC_EnableIRQ(TIMER0_IRQn);
+
+  TIMER_Enable(TIMER0, true);
 }
 
 int main(void)
@@ -121,7 +133,7 @@ int main(void)
   CHIP_Init();
   initCMU();
   initGPIO();
-  initTimer();
+  initTIMER();
 
   while (1)
   {
