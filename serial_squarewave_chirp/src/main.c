a8bee3889126d3174abe110483d0b1059a81360e
wabrams <wabrams@users.noreply.github.com>
2020-08-04 11:43:44 -0600

    fixed LDMA, rookie mistake
    didn't change the description / transmitter type appropriately

--- serial_squarewave_chirp/src/main.c
+++ serial_squarewave_chirp/src/main.c
@@ -14,105 +14,74 @@
 
 #include "retargetserial.h"
 
-int freq_start = 20000;
-int freq_stop = 30000;
-
-#define DUTY_CYCLE_STEPS  0.1
+#define DUTY_CYCLE_STEPS  0.10
 #define TARGET_DUTY_CYCLE 0.50
 
 #define LDMA_PDM_CHANNEL             0
-#define LDMA_TIMER_TOPB_CHANNEL      1
-#define LDMA_TIMER_COMP_CHANNEL      2
+#define LDMA_TIMER_COMP_CHANNEL      1
+#define LDMA_TIMER_TOPB_CHANNEL      2
 
-#define BUFFER_SIZE               (1 << 11)
-#define PP_BUFFER_SIZE                 256
+#define BUFFER_SIZE                 2048
+#define PP_BUFFER_SIZE               256
 #define TIMER_BUFFER_SIZE			       256
 
-bool prevBufferPing;
+int freq_start = 20000;
+int freq_stop = 30000;
+
 int16_t left[BUFFER_SIZE];
 int16_t right[BUFFER_SIZE];
-LDMA_Descriptor_t descLink[4];
+
 uint32_t pingBuffer[PP_BUFFER_SIZE];
 uint32_t pongBuffer[PP_BUFFER_SIZE];
+bool prevBufferPing;
 
-float pulse_width = 1e-3; //in sec
-int N_waves;
+uint32_t list_pwm[TIMER_BUFFER_SIZE];
+uint32_t list_top[TIMER_BUFFER_SIZE];
+
+float pulse_width = 5e-3; //in sec
+float dutyCycle = 0;
+int numWaves;
 uint32_t top_start, top_stop, top_value;
 uint32_t offset;
 float top_step;
 uint32_t timerFreq;
 bool play_chirp = false;
-uint32_t list_periods[TIMER_BUFFER_SIZE];
 
-typedef enum squarechirp_mode_e
+void calculateChirpLists()
 {
-	chirpInc, chirpOn, chirpDec//, chirpOff
-} squarechirp_mode_t;
-
-static volatile squarechirp_mode_t chirp_mode = chirpInc;
-static volatile float dutyCycle = 0;
-
-void TIMER0_IRQHandler(void)
-{
-	uint32_t flags = TIMER0->IF_CLR = TIMER0->IF;
-	static int counter = 0; // Keep track of square wave number
-	static int ramp_length = 0;
-	// Acknowledge the interrupt
-	if (flags & TIMER_IF_CC1)
-	{
-		/*
-		top_value = top_start + (int32_t) (counter * top_step);
-		TIMER_CompareBufSet(TIMER0, 1, (uint32_t) (top_value * dutyCycle));
-		TIMER_TopBufSet(TIMER0, top_value);
-		*/
-		switch (chirp_mode)
-		{
-      case chirpInc:
-        dutyCycle += DUTY_CYCLE_STEPS;
-        ramp_length++;
-        if (dutyCycle >= TARGET_DUTY_CYCLE)
-        {
-          chirp_mode = chirpOn;
-          dutyCycle = TARGET_DUTY_CYCLE;
-          ramp_length--;
-        }
-        break;
-      case chirpOn:
-        // I think below should be -2... or 0... -3 seems to be corrrect according to my oscilloscope
-        if (counter == (N_waves - 3 - ramp_length))
-        {
-          chirp_mode = chirpDec;
-          //printf("counter: %d\r\n", counter);
-        }
-        break;
-      case chirpDec:
-        dutyCycle -= DUTY_CYCLE_STEPS;
-        if (dutyCycle <= 0)
-        {
-          // chirp_mode = chirpOff;
-          dutyCycle = 0;
-          TIMER_Enable(TIMER0, false);
-          printf("Chirp off, counter: %d, ramp:%d\r\n", counter, ramp_length);
-          counter = -1;  // gets incremented to 0 below
-          ramp_length = 0; // reset
-          dutyCycle = DUTY_CYCLE_STEPS; // setup for when timer is restarted
-          // dutyCycle = 0;
-          chirp_mode = chirpInc;
-        }
-        break;
-		}
-		// collect all top_values used
-		list_periods[counter] = TIMER_TopGet(TIMER0);
-		counter++;
-
-		top_value = top_start + (int32_t) (counter * top_step);
-		TIMER_CompareBufSet(TIMER0, 1, (uint32_t) (top_value * dutyCycle));
-		TIMER_TopBufSet(TIMER0, top_value);
-
-	}
+  //EXTRA: clear lists, not totally necessary
+  memset(list_pwm, 0, TIMER_BUFFER_SIZE*sizeof(uint32_t));
+  memset(list_top, 0, TIMER_BUFFER_SIZE*sizeof(uint32_t));
+  // populate list using globals
+  for (int i = 0; i < numWaves; i++)
+  {
+    list_top[i] = top_start + (int)(i * top_step);
+    list_pwm[i] = list_top[i] * dutyCycle; //TODO: increment or decrement duty cycle based on i
+  }
 }
 
-void initCMU(void) {
+void calculateChirp()
+{
+  dutyCycle = TARGET_DUTY_CYCLE; //TODO: reintroduce the tapered ends with duty cycle
+  top_start = timerFreq / freq_start; // period of start frequency
+  top_stop  = timerFreq / freq_stop;  // period of stop frequency
+  numWaves = (int)(pulse_width * (freq_stop + freq_start) / 2.0) % TIMER_BUFFER_SIZE; //note the modulo to keep us in bounds of the array
+  top_step = ((float) (freq_start - freq_stop)) / ((float) (freq_start * freq_stop)) * timerFreq / (numWaves - 1);
+}
+
+void setupChirp(int prescale)
+{
+  // update the variables and lists
+	timerFreq = CMU_ClockFreqGet(cmuClock_TIMER0) / (prescale + 1);
+	calculateChirp();
+	calculateChirpLists();
+  // set first values (to be repeated)
+	TIMER_TopBufSet(TIMER0, top_start);
+  TIMER_CompareBufSet(TIMER0, 1, (uint32_t) (top_start * DUTY_CYCLE_STEPS));
+}
+
+void initCMU(void)
+{
 	CMU_ClockEnable(cmuClock_GPIO, true);
 	CMU_ClockEnable(cmuClock_PDM, true);
 	CMU_ClockSelectSet(cmuClock_PDM, cmuSelect_HFRCODPLL);
@@ -152,33 +121,6 @@ void initRTCC(void)
 	RTCC_Init(&rtccInit);
 }
 
-void setupChirp(int freq_start, int freq_stop, int prescale)
-{
-	// Start with 10% duty cycle
-	dutyCycle = DUTY_CYCLE_STEPS;
-
-	timerFreq = CMU_ClockFreqGet(cmuClock_TIMER0) / (prescale + 1);
-	top_start = timerFreq / freq_start; // period of start frequency
-	top_stop  = timerFreq / freq_stop;  // period of stop frequency
-	N_waves = pulse_width * (freq_stop+freq_start)/2;
-	top_step = ((float) (freq_start - freq_stop)) / ((float) (freq_start * freq_stop)) * timerFreq / (N_waves - 1);
-
-	printf("N_waves: %d. top_start: %lu, top_stop: %lu, top_step x1000: %d\r\n", N_waves, top_start, top_stop, (int) (top_step * 1000));
-	top_value = top_start;
-
-	// TIMER_TopSet(TIMER0, top_value);
-	// TIMER_CompareSet(TIMER0, 1, (uint32_t) (top_value * dutyCycle));
-	// Seems like when the timer is enabled, it generate an immediate interrupt
-	//    So, need to load first value into the buffer.
-	TIMER_TopBufSet(TIMER0, top_value);
-	TIMER_CompareBufSet(TIMER0, 1, (uint32_t) (top_value * dutyCycle));
-    // clear out previous list of periods
-	printf("Check first stop: %lu\r\n", TIMER_TopGet(TIMER0));
-
-	memset(list_periods, 255, TIMER_BUFFER_SIZE*sizeof(uint32_t));
-	//TODO: set new list_periods for use with LDMA
-}
-
 void initTIMER(void)
 {
 	TIMER_Init_TypeDef timer0Init = TIMER_INIT_DEFAULT;
@@ -196,10 +138,7 @@ void initTIMER(void)
 	timer0CC1Init.mode = timerCCModePWM;
 	TIMER_InitCC(TIMER0, 1, &timer0CC1Init);
 
-	setupChirp(freq_start,  freq_stop, timerPrescale1);
-
-	TIMER0->IEN = TIMER_IEN_CC1;
-	NVIC_EnableIRQ(TIMER0_IRQn);
+	setupChirp(timerPrescale1);
 }
 
 void initPDM(void)
@@ -223,6 +162,10 @@ void initPDM(void)
 
 void initLDMA(void)
 {
+  static LDMA_Descriptor_t descLink[2];
+  static LDMA_Descriptor_t timerTOPBLink;
+  static LDMA_Descriptor_t timerCOMPLink;
+
 	LDMA_Init_t init = LDMA_INIT_DEFAULT;
 	LDMA_Init(&init);
 
@@ -236,22 +179,16 @@ void initLDMA(void)
     LDMA_StartTransfer(LDMA_PDM_CHANNEL, (void*) &periTransferTx, (void*) &descLink);
 	// TIMER COMP:
     LDMA_TransferCfg_t transferCOMPConfig = LDMA_TRANSFER_CFG_PERIPHERAL(ldmaPeripheralSignal_TIMER0_CC1);
-    descLink[2] = (LDMA_Descriptor_t) LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(&list_periods,
-                                                                       &(TIMER0 -> CC[1].OCB),
-                                                                       TIMER_BUFFER_SIZE,
-                                                                       0);
-    descLink[2].xfer.size = ldmaCtrlSizeHalf;
-    descLink[2].xfer.doneIfs = 0; //no interrupts for now. TODO: should change this eventually
-    LDMA_StartTransfer(LDMA_TIMER_COMP_CHANNEL, &transferCOMPConfig, &descLink[2]);
-  // TIMER TOPB
+    timerCOMPLink = (LDMA_Descriptor_t) LDMA_DESCRIPTOR_SINGLE_M2P_BYTE(&list_pwm, &(TIMER0 -> CC[1].OCB), numWaves);
+    timerCOMPLink.xfer.size = ldmaCtrlSizeWord;
+    timerCOMPLink.xfer.doneIfs = true;
+    LDMA_StartTransfer(LDMA_TIMER_COMP_CHANNEL, &transferCOMPConfig, &timerCOMPLink);
+  // TIMER TOPB:
     LDMA_TransferCfg_t transferTOPBConfig = LDMA_TRANSFER_CFG_PERIPHERAL(ldmaPeripheralSignal_TIMER0_UFOF);
-    descLink[3] = (LDMA_Descriptor_t) LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(&list_periods,
-                                                                      &(TIMER0 -> TOPB),
-                                                                      TIMER_BUFFER_SIZE,
-                                                                      0);
-    descLink[3].xfer.size = ldmaCtrlSizeHalf;
-    descLink[3].xfer.doneIfs = 0; //no interrupts for now. TODO: should change this eventually
-    LDMA_StartTransfer(LDMA_TIMER_TOPB_CHANNEL, &transferTOPBConfig, &descLink[3]);
+    timerTOPBLink = (LDMA_Descriptor_t) LDMA_DESCRIPTOR_SINGLE_M2P_BYTE(&list_top, &(TIMER0 -> TOPB), numWaves);
+    timerTOPBLink.xfer.size = ldmaCtrlSizeWord;
+    timerTOPBLink.xfer.doneIfs = true;
+    LDMA_StartTransfer(LDMA_TIMER_TOPB_CHANNEL, &transferTOPBConfig, &timerTOPBLink);
 }
 
 void LDMA_IRQHandler(void)
@@ -262,6 +199,14 @@ void LDMA_IRQHandler(void)
 	if (pending & LDMA_IF_ERROR)
 	{
 		while (1); //TODO: assert here
+	}
+	if (pending & LDMA_IF_DONE1)
+	{
+	  LDMA_StopTransfer(1);
+	}
+	if (pending & LDMA_IF_DONE2)
+	{
+	  LDMA_StopTransfer(2);
 	}
 	if (play_chirp)
 	{
@@ -306,8 +251,7 @@ static void listen(bool snd)
 {
 	offset = 0;
 	play_chirp = snd;
-    // memset(left, 0xFFFF, BUFFER_SIZE<<1);
-    // memset(right, 0xFFFF, BUFFER_SIZE<<1);
+
 	while (offset < BUFFER_SIZE)
 	{
     EMU_EnterEM1();
@@ -388,27 +332,27 @@ int main(void)
         break;
       case 'p': // get list of periods
         printf("top_values: %d\r\n", TIMER_BUFFER_SIZE*sizeof(int));
-        binary_dump((uint8_t*)list_periods, TIMER_BUFFER_SIZE*sizeof(uint32_t));
+        binary_dump((uint8_t*)list_pwm, TIMER_BUFFER_SIZE*sizeof(uint32_t));
         printf("\r\n");
         break;
       case '0': // pulse_duration
         read_msg((uint8_t *)&pulse_width, 4);
         printf("chirp duration[ms]: %d\r\n", (int)(pulse_width*1000));
-        setupChirp(freq_start, freq_stop, timerPrescale1);
+        setupChirp(timerPrescale1);
         printf("ack\r\n");
         RETARGET_SerialFlush();
         break;
       case '1': // start frequency
         read_msg((uint8_t*)&freq_start, 2);
         printf("start: %d\r\n", freq_start);
-        setupChirp(freq_start, freq_stop, timerPrescale1);
+        setupChirp(timerPrescale1);
         printf("ack\r\n");
         RETARGET_SerialFlush();
         break;
       case '2': // stop frequency
         read_msg((uint8_t *)&freq_stop, 2);
         printf("stop: %d\r\n", freq_stop);
-        setupChirp(freq_start, freq_stop, timerPrescale1);
+        setupChirp(timerPrescale1);
         printf("ack\r\n");
         RETARGET_SerialFlush();
         break;
